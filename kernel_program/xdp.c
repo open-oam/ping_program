82237c8719d7f0bd11006c439d18ebf0629b4076
ethanbond64 <ubuntu@ip-172-31-60-215.ec2.internal>
2020-09-12 00:37:02 +0000

    Not Compiling

--- kernel_program/xdp.c
+++ kernel_program/xdp.c
@@ -8,9 +8,9 @@
 #include <linux/icmp.h>
 #include <linux/ip.h>
 #include <linux/in.h>
-#include <linux/bpf.h>
+// #include <linux/bpf.h>
 #include <linux/if_ether.h>
-
+#include <stddef.h>
 
 // Ethernet header
 // struct ethhdr {
@@ -35,6 +35,14 @@
 // } __attribute__((packed));
 
 
+
+#define IP_SRC_OFF (ETH_HLEN + offsetof(struct iphdr, saddr))
+#define IP_DST_OFF (ETH_HLEN + offsetof(struct iphdr, daddr))
+
+#define ICMP_CSUM_OFF (ETH_HLEN + sizeof(struct iphdr) + offsetof(struct icmphdr, checksum))
+#define ICMP_TYPE_OFF (ETH_HLEN + sizeof(struct iphdr) + offsetof(struct icmphdr, type))
+#define ICMP_CSUM_SIZE sizeof(__u16)
+
 // eBPF map to store IP proto counters (tcp, udp, etc)
 BPF_MAP_DEF(protocols) = {
     .map_type = BPF_MAP_TYPE_PERCPU_ARRAY,
@@ -56,9 +64,9 @@ int packet_count(struct xdp_md *ctx) {
   if (sizeof(struct ethhdr) + sizeof(struct iphdr) + sizeof(struct icmphdr) > data_end)
 		return XDP_PASS;
 
-  struct ethhdr *ether = data;
-  struct iphdr *ip_header = data + sizeof(ethhdr);
-  struct icmphdr *icmp_header = data + sizeof(ethhdr) + sizeof(iphdr);
+    struct ethhdr *ether = data;
+    struct iphdr *ip_header = data + sizeof(struct ethhdr);
+    struct icmphdr *icmp_header = data + sizeof(struct ethhdr) + sizeof(struct iphdr);
  
     // from ebpf-icmp-ping github
 	if (ether->h_proto != __constant_htons(ETH_P_IP))
@@ -76,21 +84,35 @@ int packet_count(struct xdp_md *ctx) {
     switch(icmp_header->type) {
 
     case ICMP_ECHO:
-        // log timestamp
 
-        __u8 swap[ETH_ALEN];
-        bpf_memcpy(swap, ether->h_source, ETH_ALEN);
-        bpf_memcpy(ether->h_source, ether->h_dest, ETH_ALEN);
-        bpf_memcpy(ether->h_dest, swap, ETH_ALEN);
+       /* Let's grab the MAC address.
+	 * We need to copy them out, as they are 48 bits long */
+        __u8 src_mac[ETH_ALEN];
+        __u8 dst_mac[ETH_ALEN];
+        bpf_memcpy(src_mac, ether->h_source, ETH_ALEN);
+        bpf_memcpy(dst_mac, ether->h_dest, ETH_ALEN);
 
-        __u32 swap_ip = ip_header->saddr;
-        ip_header->saddr = ip_header->daddr;
-        ip_header->daddr = swap_ip;
+        bpf_skb_store_bytes(ctx, offsetof(struct ethhdr, h_source), dst_mac, ETH_ALEN, 0);
+        bpf_skb_store_bytes(ctx, offsetof(struct ethhdr, h_dest), src_mac, ETH_ALEN, 0);
 
-        // CHECKSUM needed
+        /* Let's grab the IP addresses.
+	 * They are 32-bit, so it is easy to access */
+        __u32 src_ip = ip_header->saddr;
+        __u32 dst_ip = ip_header->daddr;
 
-        // return XDP_REDIRECT; ?
-        return XDP_DROP; 
+        /* Swap the IP addresses.
+        bpf_skb_store_bytes(ctx, IP_SRC_OFF, &dst_ip, sizeof(dst_ip), 0);
+        bpf_skb_store_bytes(ctx, IP_DST_OFF, &src_ip, sizeof(src_ip), 0);
+
+            /* Change the type of the ICMP packet to 0 (ICMP Echo Reply).
+        * This changes the data, so we need to re-calculate the checksum
+        */
+        __u8 new_type = ICMP_ECHOREPLY;
+        /* We need to pass the full size of the checksum here (2 bytes) */
+        bpf_l4_csum_replace(ctx, ICMP_CSUM_OFF, ICMP_ECHO, new_type, ICMP_CSUM_SIZE);
+        bpf_skb_store_bytes(ctx, ICMP_TYPE_OFF, &new_type, sizeof(new_type), 0);
+
+        return XDP_TX; 
 
     case ICMP_ECHOREPLY:
 
