23164b5142ad3bdad1cfc66a1f77ab272a396a65
vincent <vincent.morgan.42@gmail.com>
2020-09-14 14:26:59 -0600

    Cleaned bpf code and added more comments

--- kernel_program/xdp.c
+++ kernel_program/xdp.c
@@ -12,8 +12,6 @@
 #include <linux/if_ether.h>
 #include <stddef.h>
 
-#define bpf_memcpy __builtin_memcpy
-
 #define IP_SRC_OFF (ETH_HLEN + offsetof(struct iphdr, saddr))
 #define IP_DST_OFF (ETH_HLEN + offsetof(struct iphdr, daddr))
 
@@ -24,85 +22,96 @@
 #define ICMP_ECHO_LEN		64
 
 
-
+//Underlying BPF map for perf-events
 BPF_MAP_DEF(perfmap) = {
     .map_type = BPF_MAP_TYPE_PERF_EVENT_ARRAY,
     .max_entries = 128,
 };
 BPF_MAP_ADD(perfmap);
 
-
+// Perf-event data struct
 struct perf_event_item {
   __u32 data;
 };
 // _Static_assert(sizeof(struct perf_event_item) == 12, "wrong size of perf_event_item");
 
 
-static __always_inline __u16 csum_fold_helper(__wsum sum) {
-	sum = (sum & 0xffff) + (sum >> 16);
-	return ~((sum & 0xffff) + (sum >> 16));
-}
+// static __always_inline __u16 csum_fold_helper(__wsum sum) {
+// 	sum = (sum & 0xffff) + (sum >> 16);
+// 	return ~((sum & 0xffff) + (sum >> 16));
+// }
 
-static __always_inline __u16 ipv4_csum(void *data_start, int data_size ) {
-	__wsum sum;
+// Recomputes csum using bpf call and returns new csum
+// static __always_inline __u16 ipv4_csum(void *data_start, int data_size ) {
+// 	__wsum sum;
 
-	sum = bpf_csum_diff(0, 0, data_start, data_size, 0);
-	return csum_fold_helper(sum);
-}
+// 	sum = bpf_csum_diff(0, 0, data_start, data_size, 0);
+// 	// return csum_fold_helper(sum);
+//   return sum;
+// }
 
 
 SEC("xdp_prog")
 int packet_count(struct xdp_md *ctx) {
+  //Get pointers into pointer type
   void *data_end = (void *)(long)ctx->data_end;
   void *data = (void *)(long)ctx->data;
-
-  // Only IPv4 supported for this example
   
+  //Checks size of packet to make verifier content
   if (sizeof(struct ethhdr) + sizeof(struct iphdr) + sizeof(struct icmphdr) > data_end) {
 		return XDP_PASS;
   }
 
+  //Point ethernet, ip, and icmp headers at ctx in order
   struct ethhdr *eth_header = data;
   struct iphdr *ip_header = data + sizeof(struct ethhdr);
   struct icmphdr *icmp_header = data + sizeof(struct ethhdr) + sizeof(struct iphdr);
  
+  //If packet is not IP, then pas
 	if (eth_header->h_proto != __constant_htons(ETH_P_IP)) {
 		return XDP_PASS;
   }
 
+  //If packet is not ICMP, then pass
 	if (ip_header->protocol != IPPROTO_ICMP) {
 		return XDP_PASS;
   }
 
+  //If ICMP packet is of type ECHO REQUEST, then reply 
   if (icmp_header->type == ICMP_ECHO) {
+
+    //Get MAC addresses
     __u8 src_mac[ETH_ALEN];
     __u8 dst_mac[ETH_ALEN];
-
     memcpy(src_mac, eth_header->h_source, ETH_ALEN);
     memcpy(dst_mac, eth_header->h_dest, ETH_ALEN);
 
+    //Swap MAC addresses
     bpf_skb_store_bytes(ctx, offsetof(struct ethhdr, h_source), dst_mac, ETH_ALEN, 0);
     bpf_skb_store_bytes(ctx, offsetof(struct ethhdr, h_dest), src_mac, ETH_ALEN, 0);
 
+    //Get IP addresses
     __u32 src_ip = ip_header->saddr;
     __u32 dst_ip = ip_header->daddr;
 
+    //Swap IP addresses
     bpf_skb_store_bytes(ctx, IP_SRC_OFF, &dst_ip, sizeof(dst_ip), 0);
     bpf_skb_store_bytes(ctx, IP_DST_OFF, &src_ip, sizeof(src_ip), 0);
 
+    //Recompute IP checksum and save to context
+    __u8 new_type = ICMP_ECHOREPLY;
+    bpf_l4_csum_replace(ctx, ICMP_CSUM_OFF, ICMP_ECHO, new_type, ICMP_CSUM_SIZE);
+    bpf_skb_store_bytes(ctx, ICMP_TYPE_OFF, &new_type, sizeof(new_type), 0);
 
-    // __u8 new_type = ICMP_ECHOREPLY;
-    // bpf_l4_csum_replace(ctx, ICMP_CSUM_OFF, ICMP_ECHO, new_type, ICMP_CSUM_SIZE);
-    // bpf_skb_store_bytes(ctx, ICMP_TYPE_OFF, &new_type, sizeof(new_type), 0);
-
-    icmp_header->type = ICMP_ECHOREPLY;
-    icmp_header->checksum = 0;
-    icmp_header->checksum = ipv4_csum(icmp_header, ICMP_ECHO_LEN);
+    // icmp_header->type = ICMP_ECHOREPLY;
+    // icmp_header->checksum = 0;
+    // icmp_header->checksum = ipv4_csum(icmp_header, ICMP_ECHO_LEN);
 
     return XDP_PASS;
-
-  } else if (icmp_header->type == ICMP_ECHOREPLY) {
-
+  }
+  
+  //If ICMP packet is of type ECHO REPLY, then send perf event up to userspace
+  if (icmp_header->type == ICMP_ECHOREPLY) {
 
     struct perf_event_item evt = {
       .data = 0
@@ -121,8 +130,6 @@ int packet_count(struct xdp_md *ctx) {
     __u64 flags = BPF_F_CURRENT_CPU | (data_end - data << 32);
     bpf_perf_event_output(ctx, &perfmap, flags, &evt, sizeof(evt));
     return XDP_PASS;
-
-
   }
 
   return XDP_PASS;
